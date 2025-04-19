af658714504c975f93fb334292cb882e28cc59a2
deciob <decio.battaglia@gmail.com>
2025-04-19 19:54:15 +0100

    Changes to config to try to improve Blink and Windsurf and avoid the hijacking of the escape button

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -27,7 +27,7 @@ return {
 		-- C-k: Toggle signature help (if signature.enabled = true)
 		--
 		-- See :h blink-cmp-config-keymap for defining your own keymap
-		keymap = { preset = "default", ["<CR>"] = { "accept", "fallback" } },
+		keymap = { preset = "default", ["<CR>"] = { "accept", "fallback" }, ["<F1>"] = { "cancel", "fallback" } },
 
 		appearance = {
 			-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
@@ -36,7 +36,7 @@ return {
 		},
 
 		-- (Default) Only show the documentation popup when manually triggered
-		completion = { documentation = { auto_show = true }, ghost_text = { enabled = true } },
+		completion = { documentation = { auto_show = true }, ghost_text = { enabled = false } },
 
 		-- Blink supports signature help, automatically triggered when typing trigger characters, defined by the LSP, such as ( for lua.
 		-- The menu will be updated when pressing a retrigger character, such as ,.
--- nvim/lua/plugins/windsurf.lua
+++ nvim/lua/plugins/windsurf.lua
@@ -3,33 +3,21 @@
 return {
 	"Exafunction/windsurf.vim",
 	event = "BufEnter",
-	init = function()
-		local function setup()
-			local keymap = vim.keymap.set
-
-			vim.g.codeium_enabled = false
-			keymap("n", "<leader><llm>", ":CodeiumToggle<CR>")
-
-			-- -- Accept suggestion
-			vim.keymap.set("i", "<C-a>", function()
-				return vim.fn["codeium#Accept"]()
-			end, { expr = true, silent = true })
-
-			-- Next suggestion
-			keymap("i", "<C-]>", function()
-				return vim.fn["codeium#CycleCompletions"](1)
-			end, { expr = true, desc = "Codeium: Next suggestion" })
-
-			-- Previous suggestion
-			keymap("i", "<C-[>", function()
-				return vim.fn["codeium#CycleCompletions"](-1)
-			end, { expr = true, desc = "Codeium: Prev suggestion" })
-
-			-- -- Clear suggestions
-			-- keymap("i", "<C-x>", function() return vim.fn["codeium#Clear"]() end,
-			--     { expr = true, desc = "Codeium: Clear suggestions" })
-		end
-
-		setup()
+	config = function()
+		vim.g.codeium_enabled = false
+		-- Map <leader>-llm to toggle Codeium
+		vim.keymap.set("n", "<C-l>", ":Codeium Toggle<CR>", { noremap = true, silent = false })
+		vim.keymap.set("i", "<C-a>", function()
+			return vim.fn["codeium#Accept"]()
+		end, { expr = true, silent = true })
+		vim.keymap.set("i", "<C-;>", function()
+			return vim.fn["codeium#CycleCompletions"](1)
+		end, { expr = true, silent = true })
+		vim.keymap.set("i", "<C-,>", function()
+			return vim.fn["codeium#CycleCompletions"](-1)
+		end, { expr = true, silent = true })
+		vim.keymap.set("i", "<C-x>", function()
+			return vim.fn["codeium#Clear"]()
+		end, { expr = true, silent = true })
 	end,
 }
